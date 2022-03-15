10588f9c5f7a0205565c0ea3b753b305cab2509e
Rutzno <d.rutzno@gmail.com>
2022-03-15 16:43:57 +0000

    Stage 4

--- src/main/java/ml/diarpy/addressapp/MainApp.java
+++ src/main/java/ml/diarpy/addressapp/MainApp.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -13,7 +14,11 @@ import javafx.stage.Stage;
 import ml.diarpy.addressapp.controller.PersonEditDialog;
 import ml.diarpy.addressapp.controller.PersonOverview;
 import ml.diarpy.addressapp.model.Person;
+import ml.diarpy.addressapp.model.PersonListWrapper;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.IOException;
 import java.util.prefs.Preferences;
@@ -143,6 +148,54 @@ public class MainApp extends Application {
         } else {
             preferences.remove("filePath");
             this.primaryStage.setTitle("AddressApp");
+        }
+    }
+
+    public void loadPersonDataFromFile(File file) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            // Reading XML from the file and unmarshalling
+            PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+
+            personData.clear();
+            personData.addAll(wrapper.getPersons());
+
+            // Save the file path to the registry
+            setPersonFilePath(file);
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Couldn't load data");
+            alert.setContentText("Couldn't load data from file:\n" + file.getPath());
+
+            alert.showAndWait();
+        }
+    }
+
+    public void savePersonDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(personData);
+
+            // Marshalling and saving XML to the file
+            m.marshal(wrapper, file);
+
+            // Save the person file to the registry
+            setPersonFilePath(file);
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Couldn't save data");
+            alert.setContentText("Couldn't save data from file:\n" + file.getPath());
+
+            alert.showAndWait();
         }
     }
 }
